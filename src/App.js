f86b548f7bd64e54c54aff79c29612df8a614d7f
Justinas Kairys <info@ibnz.co.uk>
2021-05-10 10:18:02 +0100

    added dummies

--- src/App.js
+++ src/App.js
@@ -22,6 +22,12 @@ const DUMMY_EXPENSES = [
     amount: 450,
     date: new Date(2021, 5, 12),
   },
+  {
+    id: 'e5',
+    title: 'Laptop',
+    amount: 450,
+    date: new Date(2020, 5, 12),
+  },
 ];
 
 const App = () => {
