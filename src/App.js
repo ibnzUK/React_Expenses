d3e6a700e6ba3fabf60cf984fe90143113f50dfd
Justinas Kairys <info@ibnz.co.uk>
2021-05-10 09:13:29 +0100

    dummy expenses changed

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import { useState } from 'react';
 const DUMMY_EXPENSES = [
   {
     id: 'e1',
-    title: 'Pen',
+    title: 'Ball',
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: 'e2', title: 'Bike', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: 'e2', title: 'Motorbike', amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: 'e3',
     title: 'Car Insurance',
